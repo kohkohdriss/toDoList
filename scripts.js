3b5a78b22b4244581ffc5352fa71a696ee6051af
driss <kohkoh.driss@gmail.com>
2023-01-10 00:20:53 +0100

    localStorage Lifcycle Hooks
    set, get, and update data in localstorage,  use lifcyle hooks

--- scripts.js
+++ scripts.js
@@ -14,13 +14,31 @@ const todosApp = {
         this.newTodo = {
           done: false,
         };
+        localStorage.setItem("todos", JSON.stringify(this.todos));
       } else {
+        //    var loc = localStorage.getItem("todos", this.todos);
+        //    console.log(object);
         alert("to-do text is required");
       }
     },
-    clear() {
-      this.todos = [];
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+      console.log("updated");
     },
+  },
+  //   beforeCreate() {
+  //     let dat = new Date();
+  //     console.log("Before create");
+  //     console.log(dat.getMilliseconds());
+  //     console.log(this.newTodo);
+  //   },
+  created() {
+    this.todos = localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : this.todos;
+  },
+  updated() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   },
 };
 
