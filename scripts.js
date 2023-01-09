b37324394da749981952cc9bce3de5b4f1bc09aa
driss <kohkoh.driss@gmail.com>
2023-01-09 16:03:28 +0100

    configuration of the "clear" button

--- scripts.js
+++ scripts.js
@@ -1,18 +1,7 @@
-var todos = [
-  {
-    text: "Learn HTML, CSS and Javascript",
-    done: true,
-  },
-  {
-    text: "Learn the basics of Vue JS",
-    done: false,
-  },
-];
-
 const todosApp = {
   data() {
     return {
-      todos: window.todos,
+      todos: [],
       newTodo: {
         done: false,
       },
